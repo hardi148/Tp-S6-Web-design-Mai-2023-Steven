86b037dac80a6f4caaaf59598dcfb367e9e4eb29
hardi148 <tojohajarisoa@gmail.com>
2023-05-02 14:45:03 +0100

    update

--- TP S6 Web design Mai 2022/resources/views/user/ListeArticle_user.blade.php
+++ TP S6 Web design Mai 2022/resources/views/user/ListeArticle_user.blade.php
@@ -235,37 +235,8 @@
       <div><button class="btn btn-primary" type="submit">Ajouter Article</button></div>
     </form>
   </div>
-  <script>
-    let slideIndex = 1;
-    showSlides(slideIndex);
-
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {
-        slideIndex = 1
-      }
-      if (n < 1) {
-        slideIndex = slides.length
-      }
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].className += " active";
-    }
+  <script src="Utile/sliders.js">
+    
   </script>
   <script src="Utile/utile.js"></script>
   <script src="Utile/uploadImg.js"></script>
